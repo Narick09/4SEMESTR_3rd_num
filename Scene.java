8fa17d39f4696e056ee89fc8b0d81ff0b0ea6bbb
void <49158033+Narick09@users.noreply.github.com>
2020-05-21 17:14:55 +0500

    Update Scene.java

--- Scene.java
+++ Scene.java
@@ -1,38 +1,48 @@
 package ThirdNumPack;
-
 import javax.swing.*;
 import java.awt.*;
 import java.io.IOException;
 import java.util.ArrayList;
 
-//мб добавить класс, к-ый будет хранить в себе местоположения и начальные координаты картинок
-//и добавить в пикчу конструктор, принимающий массив этих классов
-
 public class Scene extends JPanel
 {
     private ArrayList<Picture> pictures = new ArrayList<>();
-    public Scene() throws IOException {
+    public Scene(Model S) throws IOException {
         pictures.add(new Picture("C:\\Users\\Даниил\\IdeaProjects\\3rdNum\\out\\production\\3rdNum\\ThirdNumPack\\Pictures\\Background.jpg",0, 0));
-        pictures.add(new Picture("C:\\Users\\Даниил\\IdeaProjects\\3rdNum\\out\\production\\3rdNum\\ThirdNumPack\\Pictures\\BattleShip.png",450, 550));
+        pictures.add(S.getPicture());
+        for(Picture pic: pictures){
+            pic.setBounds(pic.getX(), pic.getY(), pic.getWidth(), pic.getHeight());
+            this.add(pic);
+        }
+        repaint();
+        setVisible(true);
     }
-    public Scene(ArrayList<Picture> pictures){
-        this.pictures = pictures;
-    }
+//    public Scene(ArrayList<Picture> pictures){
+//        this.pictures = pictures;
+//    }
 
     public void moveElement(int index, Coordinates coordinates){
         pictures.get(index).setCoordinates(coordinates.getX(), coordinates.getY());
+        //pictures.get(index).setBounds(coordinates.getX(), coordinates.getY(), pictures.get(index).getWidth(), pictures.get(index).getHeight());
+        repaint();
+    }
+    public void addToScene(String PictureFileName, int x, int y) throws IOException {
+        this.pictures.add(new Picture(PictureFileName, x, y));
     }
     public void setScene(ArrayList<Picture> pictures){
         if(pictures != null){
             this.pictures = pictures;
         }
     }
-    public void paintComponent(Graphics g)
+    public void move(){
+
+    }
+
+    public void paint(Graphics g)
     {
         for (Picture picture : pictures) {
             g.drawImage(picture.getImage(), picture.getX(), picture.getY(), null);
         }
     }
-
     //private ArrayList<Image> images = new ArrayList<>();
 }
