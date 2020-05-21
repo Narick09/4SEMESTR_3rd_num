1e35e7d8dd29c3d16a7676803dad83bc52a1c619
void <49158033+Narick09@users.noreply.github.com>
2020-05-21 17:19:06 +0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -5,20 +5,11 @@ import java.io.IOException;
 public class Game {
     public void gaming(){
         try {
-            Model Spaceship = new ModelStarShip(500, 800);//initialisation
+            Model Spaceship = new ModelStarShip(0, 0, 50);//initialisation
             Controller Control = new StandartController(Spaceship);//initialisation
-            View Interface = new StandartView(Spaceship, Control, 1200,900);
-            Interface.openWindow();
-            Interface.drawMenu();
-            Interface.drawInitGameProcess();
-            //Control.controlling();
-
+            View Interface = new StandartView(Spaceship, Control, 1200,800);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    //игра:
-    //модель корабля вверх - стрелять на опр клавишу - через акшион листенер
-    //надо будет передавать координаты положения корабля, чтобы знать, откуда производить выстрел
-    //этим будет занимать контроллер
 }
