fe2ddac8d44803d02ed50466813b0053dfe67fdb
void <49158033+Narick09@users.noreply.github.com>
2020-05-21 17:20:17 +0500

    Update StandartController.java

--- StandartController.java
+++ StandartController.java
@@ -1,9 +1,5 @@
 package ThirdNumPack;
 
-
-import java.io.IOException;
-
-
 public class StandartController implements Controller {
     private boolean flag_game_started = false;
     private boolean flag_game_pause = false;
@@ -11,7 +7,7 @@ public class StandartController implements Controller {
     private boolean flag_exit = false;
     private int leftBorder = 0;
     private int rightBorder = 1200;
-    private int downBorder = 900;
+    private int downBorder = 800;
     private int upBorder = 0;
     //private View Viewer = null;
     private Model SpaceShip = null;
@@ -21,42 +17,25 @@ public class StandartController implements Controller {
         this.SpaceShip = Ship;
     }
 
-    public void controlling() throws IOException{
-        int r = 0;
-        while(r < 100){
-            this.SpaceShip.moveRight();
-        }
-        //destroyEnemies();
-    }
-//    public void move(){
-//
+//    public void controlling() throws IOException{
+//        int r = 0;
+//        while(r < 100){
+//            this.SpaceShip.moveRight();
+//        }
+//        //destroyEnemies();
 //    }
     public void moving(boolean Right, boolean Left, boolean Up, boolean Down){
-        if(Right && !Left && !Up && !Down)
-            if(SpaceShip.getX() + SpaceShip.getShipWidth() < rightBorder)
+        if(Right)
+            if((SpaceShip.getX() + SpaceShip.getShipWidth()) + 10 < rightBorder)
                 SpaceShip.moveRight();
-        if(!Right && Left && !Up && !Down)
+        if(Left)
             if(SpaceShip.getX() > leftBorder)
                 SpaceShip.moveLeft();
-        if(!Right && !Left && Up && !Down)
-            if(SpaceShip.getX() > upBorder)
+        if(Up)
+            if(SpaceShip.getY() > upBorder)
                 SpaceShip.moveUp();
-        if(!Right && !Left && !Up && Down)
-            if(SpaceShip.getX() + SpaceShip.getShipHeight() < downBorder)
+        if(Down)
+            if(SpaceShip.getY() + SpaceShip.getShipHeight() + 10 < downBorder)
                 SpaceShip.moveDown();
-    }
-    public void destroyEnemies() throws IOException {//moving & shooting
-        //while(!flag_exit && !flag_game_pause){
-
-
-//        while(Viewer.drawGameScene(i)){
-//             if(scan.next().equals("d"))
-//                 break;
-//             i++;
-//            //Thread.sleep(500);
-//            //KeyListener
-//            //в разных слушателях continue
-//            //добавить методы управления в модель. Последняя будет связываться с вивером и передавать данные об изменении положения.
-//        }
     }
 }
