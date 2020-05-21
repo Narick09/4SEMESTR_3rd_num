3abcdc5328f7c74210995643a52282b7fe8e878d
void <49158033+Narick09@users.noreply.github.com>
2020-05-21 17:18:20 +0500

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -3,6 +3,6 @@ package ThirdNumPack;
 import java.io.IOException;
 
 public interface Controller {
-    void controlling() throws IOException;
+    //void controlling() throws IOException;
     void moving(boolean Right, boolean Left, boolean Up, boolean Down);
 }
