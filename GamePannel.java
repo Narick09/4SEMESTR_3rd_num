e702cec1ce25ede7c73850ee91703905c578619b
void <49158033+Narick09@users.noreply.github.com>
2020-05-21 17:14:04 +0500

    Update GamePannel.java

--- GamePannel.java
+++ GamePannel.java
@@ -1,27 +1,29 @@
 package ThirdNumPack;
 
 import javax.swing.*;
-import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class GamePannel extends JFrame {
-    //private JPanel panel = null;
     private Scene presentScene = null;
-    public GamePannel() throws IOException {
+    public GamePannel(Model S) throws IOException {
+        //System.out.println("GamePannel constructor");
         setTitle("GAMA Bullet");
         setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
         //this.panel = new JPanel();
         // Добавление компонента к фрейму.
-        presentScene  = new Scene();
+        presentScene  = new Scene(S);
         this.add(presentScene);
+        setVisible(true);
     }
     public GamePannel(Scene scene) throws IOException {
+        //System.out.println("GamePannel constructor2");
         setTitle("GAMA Bullet");
         setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
         // Добавление компонента к фрейму.
         presentScene  = scene;
         this.add(presentScene);
+
     }
     public Scene getScene(){
         return this.presentScene;
@@ -30,24 +32,21 @@ public class GamePannel extends JFrame {
         if(component != null) {
             this.remove(this.presentScene);
             this.presentScene = component;
-            //this.presentScene.setScene();
             this.add(this.presentScene);
-            //this.setVisible(true);
+            this.setVisible(true);
+            repaint();
+            //System.out.println("GamePannel reDraw");
         }
     }
-//    public void Add(ActionListener component){
-//        this.addL(component);
-//    }
     public void reDraw(ArrayList<Picture> pictures){
         this.presentScene.setScene(pictures);
         this.reDraw(presentScene);
     }
-    //public boolean reDraw(int index)
     public void reDraw(int index, Coordinates coordinates){
+        System.out.println("GamePannel reDrawIndex");
         this.presentScene.moveElement(index, coordinates);
-        this.reDraw(presentScene);
+        repaint();
     }
     public static final int DEFAULT_WIDTH = 1200;
     public static final int DEFAULT_HEIGHT = 900;
 }
-
